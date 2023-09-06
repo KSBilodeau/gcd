fcb34349b02b303a4b56013751fd0d8041b2a6f0
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2023-09-05 22:28:39 -0500

    Document integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -21,38 +21,53 @@ fn middle_school_undefined() {
     assert_eq!(Err("GCD undefined for 0 and 0"), middle_school_gcd(0, 0));
 }
 
+// Euclid's method should return b when a is 0, which this confirms
 #[test]
 fn euclid_zero_a() {
     assert_eq!(Ok(2), euclid_gcd(0, 2));
 }
 
+// Euclid's method should also return a when b is 0, which this confirms
 #[test]
 fn euclid_zero_b() {
     assert_eq!(Ok(2), euclid_gcd(2, 0));
 }
 
+// Tests the euclid_gcd against all permutations of 100 consecutive numbers
 #[test]
 fn euclid_one_to_hundred() {
     for a in 1..=100 {
         for b in 1..=100 {
+            // Brings in a trusted 3rd party dependency to check against
+            // Under dev deps so it is not utilized in the actual library code
             assert_eq!(Ok(num_integer::gcd(a, b)), euclid_gcd(a, b));
         }
     }
 }
 
+// Tests the euclid_gcd against 100 random pairs of numbers ranging from 1 to
+// 18_446_744_073_709_551_615
 #[test]
 fn euclid_random() {
+    // Trusted 3rd party dependency used to generate random numbers
     use rand::Rng;
 
+    // Create the random number generator
     let mut rng = rand::thread_rng();
 
     for _ in 0..100 {
+        // Generate the first random number
         let a: u64 = rng.gen_range(1..u64::MAX);
+        // Then the second random number
         let b: u64 = rng.gen_range(1..u64::MAX);
 
+        // Finally compare the two algorithms to ensure they match
         assert_eq!(Ok(num_integer::gcd(a, b)), euclid_gcd(a, b));
     }
 }
+
+// From here on out, the tests follow the same pattern as euclid_gcd with minor alterations
+// to account for different preconditions.
 
 #[test]
 fn consecutive_zero_a() {
